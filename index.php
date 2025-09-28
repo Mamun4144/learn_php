9d0c1e42185c0db43dd83d3ffe7677e536881cce
Mamun4144 <mamunit4144@gmail.com>
2025-09-28 18:57:02 +0600

    php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    // $color = "red";
+    // echo "My car is " . $color . "<br>"; // currect
+    // echo "My House is " . $Color . "<br>"; //wrong
+    // echo "My boat is ". $COLOR. "<br>";// wrong
+    
+
+
+
+
+    // $x = 5;
+    // $y = "john";
+    
+    // echo $x;
+    // echo "<br>";
+    // echo $y;
+    
+//     $txt = "My son";
+//     echo  "I love $txt !";
+//    echo " <br>";
+    
+//     $x = "5";
+//     $y = "2";
+//     echo $x+ $y;
+
+
+
+
+$age = 25;
+$name = "Mamun";
+$fruits = ["Mango", "Banana", "Apple"];
+
+var_dump($age);
+echo"<br>";
+var_dump($name);
+echo "<br>";
+var_dump($fruits);
+        ?>
+</body>
+
+</html>
